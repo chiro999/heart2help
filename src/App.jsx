0d1eeb3280972ba8a649778e0ca6367e58b9af31
Nana Kwame <105567071+chiro999@users.noreply.github.com>
2024-07-10 04:38:46 +0000

    Update App.jsx (routing)

--- src/App.jsx
+++ src/App.jsx
@@ -82,7 +82,7 @@ function App() {
         fetchJobs();
       } else {
         setIsAuthenticated(false);
-        window.location.href = '../public/login.html';
+        window.location.href = '../login.html';
       }
       setLoading(false);
     });
